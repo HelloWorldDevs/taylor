3a62576acb46ca74a1253b553c3bb45cf1b735cb
Joe Karasek <joseph.karasek@gmail.com>
2016-08-18 17:54:08 -0700

    clean up the carousel content

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@
       animationSpeed: Number(600),
       pauseOnHover: true,
       video: false,
-      controlNav: true,
+      controlNav: false,
       directionNav: false,
       smoothHeight: false,
       useCSS: true,
@@ -55,10 +55,41 @@
     });
   };
 
+  HelloWorldDevs.prototype.mailForm = function (form) {
+    var $form = $(form);
+    $form.before('<div class="form-error"></div>');
+    $form.submit(function(e) {
+      e.preventDefault();
+      var formData = $form.serialize();
+      var formAction = $form.attr('action');
+      $.ajax({
+        type: 'POST',
+        url: formAction,
+        data: formData,
+        dataType: 'json',
+        encode: true
+      }).done(function (response) {
+        $('.form-error').remove();
+        $form.replaceWith('Congratulations! Dentistry is a big part of a \
+	            healthy life, and we\'re excited to be a part of yours. We will \
+	            contact you in the next 2 business days to schedule your \
+	            appointment and to answer any questions you may still have. \
+	            Thank you!');
+      }).error(function (response) {
+        var $error_list = $('<ul></ul>');
+        $.each(response.responseJSON, function(key, value) {
+          $error_list.append('<li>'+value+'</li>');
+        });
+        $('.form-error').html($error_list).fadeIn();
+      });
+    });
+  };
+
   var HWD = new HelloWorldDevs();
   $(document).ready(function () {
     HWD.fixRows();
     HWD.noOrphans('h1,h2,h3,h4,h5,h6,li,p');
+    HWD.mailForm('#mail-form');
   });
   $(window).on("resize", function () {
     HWD.fixRows();
